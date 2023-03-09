2fd6dbbc03880865ba038c83f7d3a8a06c897d94
MrWyss-MSFT <marius.wyss@microsoft.com>
2023-03-09 09:21:31 +0100

    ConfigMgr 2111 new bitmask and smaller queries

--- Create-CoMgmtWMIFilterMOFFiles.ps1
+++ Create-CoMgmtWMIFilterMOFFiles.ps1
@@ -69,14 +69,14 @@ instance of MSFT_Rule
     #endregion
     #region CoManagementFlag Enum
     [Flags()] enum CoManagementFlag {
-        CoManagementConfigured = 1
-        CompliancePolicies = 3
-        ResourceAccessPolicies = 5
-        DeviceConfiguration = 9
-        WindowsUpdatesPolicies = 17
-        EndpointProtection = 33
-        ClientApps = 65
-        OfficeClickToRunApps = 129
+        CompliancePolicies = 2
+        ResourceAccessPolicies = 4
+        DeviceConfiguration = 8
+        WindowsUpdatesPolicies = 16
+        ClientApps = 64
+        OfficeClickToRunApps = 128
+        EndpointProtection = 4128
+        CoManagementConfigured = 8193
     }
     #endregion
     
@@ -90,28 +90,57 @@ instance of MSFT_Rule
 
 Process {
     Write-Host "Generating MOF Files" -ForegroundColor Green
+    # Loop all Workloads except CoManagementConfigured
     foreach ( $Workload in [CoManagementFlag].GetEnumNames() | Where-Object { $_ -ne [CoManagementFlag]::CoManagementConfigured } ) {
-        # Loop all Workloads except CoManagementConfigured
         
+        # Loop all Workload Configs (intune or ConfigMgr)
         Write-Host "Workload $Workload" -ForegroundColor Magenta
         foreach ($WorkloadConfig in $WorkloadConfigs) {
-            #Loop all Workload Config (Intune and ConfigMgr or Both)
-
-            # Gets all the Decimals where the current workload is flagged
-            $WorkloadNumbers = (1..255).where({ (($_ -band [CoManagementFlag]::$Workload) -eq [CoManagementFlag]::$Workload) })
             
-            $WQLQuery = "Select ComgmtWorkloads from CCM_System Where `n (ComgmtWorkloads "
-            if ($WorkloadConfig -eq "ConfigMgr") {
-                # Builds the wql where part, (NOT IN), Filter would match if workload is set to ConfigMgr
-                # (ComgmtWorkloads != 65) and (ComgmtWorkloads != 67) ...
-                $WQLQuery += "!= " + ($WorkloadNumbers -Join (") and `n (ComgmtWorkloads != ")) + ")"
-            }
+            # Build WQL Query and set the CheckBit for the binary representation
             if ($WorkloadConfig -eq "Intune") {
-                # Builds the wql where part, (IN), Filter would match if workload is set to Intune
-                # (ComgmtWorkloads = 65) or (ComgmtWorkloads = 67) ...
-                $WQLQuery += "= " + ($WorkloadNumbers -Join (") or `n (ComgmtWorkloads = ")) + ")"
+                $CheckBit = "1"
+                $WQLWhereQuery1 = "= "
+                $WQLWhereQuery2 = ") or `n (ComgmtWorkloads = "
             }
+            if ($WorkloadConfig -eq "ConfigMgr") { 
+                $CheckBit = "0"
+                $WQLWhereQuery1 = "!= "
+                $WQLWhereQuery2 = ") and `n (ComgmtWorkloads != "
+            }
+            $WQLQuery = "Select ComgmtWorkloads from CCM_System Where `n (ComgmtWorkloads "
 
+            # Get all keys from the hashtable except for current Workload
+            $keys = [CoManagementFlag].GetEnumNames() | Where-Object { $_ -ne $Workload }
+        
+            # Create an empty array list to store the sums of each combination
+            $combinations = [System.Collections.ArrayList]@()
+        
+            # Loop over all possible combinations using binary representation of numbers
+            for ($i = 0; $i -lt [Math]::Pow(2, $keys.Count); $i++) {
+            
+                # Convert the current number to binary and pad it with zeros on the left to match the number of keys
+                $binaryStr = [Convert]::ToString($i, 2).PadLeft($keys.Count, '0')
+            
+                # Initialize the sum with the value of the current Workload
+                $sum = [CoManagementFlag]::$Workload.value__
+            
+                # Loop over each character in the binary string and check if it's '1'
+                for ($j = 0; $j -lt $binaryStr.Length; $j++) {
+
+                    if ($binaryStr[$j] -eq $CheckBit) {
+                        # If it's the CheckBit value 1 or 0, add the corresponding value from the hashtable to the sum
+                        $sum += [CoManagementFlag]::$($keys[$j]).value__
+                    }
+                }
+            
+                # Add a new property to the array list with a unique name and set its value to the calculated sum 
+                $combinations.add($sum) | Out-Null
+            }
+        
+            # Build query for each combination
+            $WQLQuery += $WQLWhereQuery1 + ($combinations -Join $WQLWhereQuery2) + ")"
+            
             Write-Verbose "$WorkloadConfig Query: `n $WQLQuery"
             
             # Generates the WMI Filer MOF Files
